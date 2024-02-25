afb20dc059feb2f0db2ec49236a03beaa07194d1
Wulu Knut <wuluknut@gmail.com>
2024-02-25 19:03:39 +0800

    feat(erupt): 添加菜单初始化类

---
+++ tongliao-oj-erupt/src/main/java/com/wuluknut/tongliaooj/config/EruptConfig.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2024 Wulu Knut
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.wuluknut.tongliaooj.config;
+
+import org.springframework.context.annotation.Configuration;
+import xyz.erupt.core.module.EruptModule;
+import xyz.erupt.core.module.EruptModuleInvoke;
+import xyz.erupt.core.module.MetaMenu;
+import xyz.erupt.core.module.ModuleInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Erupt Framework 配置类
+ *
+ * @author <a href="mailto:wuluknut@gmail.com" rel="nofollow">wuluknut</a>
+ * @version 0.0.0
+ */
+@Configuration
+public class EruptConfig implements EruptModule {
+
+
+    static {
+        EruptModuleInvoke.addEruptModule(EruptConfig.class);
+    }
+
+    @Override
+    public ModuleInfo info() {
+        return ModuleInfo.builder().name("web-oj").build();
+    }
+
+    @Override
+    public List<MetaMenu> initMenus() {
+        List<MetaMenu> menus = new ArrayList<>();
+
+        return menus;
+    }
+}
