e15bcc4f75e939b42e4a4513b42000d67c529346
Wulu Knut <wuluknut@gmail.com>
2024-04-05 16:08:32 +0800

    feat(webui): 添加代码编辑器布局

---
+++ tongliao-oj-webui/src/layouts/code.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const { width } = useWindowSize()
+
+if (width.value < 768) {
+  useRouter().push('/err')
+}
+
+watchThrottled(
+  width,
+  (w) => {
+    if (w < 768) {
+      useRouter().push('/err')
+    }
+  },
+  {
+    throttle: 500,
+  },
+)
+</script>
+
+<template>
+  <div h-full bg-gray-100 dark:bg-black:100>
+    <el-container h-full>
+      <el-main>
+        <slot />
+      </el-main>
+    </el-container>
+  </div>
+</template>
